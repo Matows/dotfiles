9530421bb98adb4aa581122f7d742daf886d1f5b
Simon LÉONARD <Matows@users.noreply.github.com>
2020-06-23 04:36:58 +0200

    add picom (transparency)

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -9,3 +9,4 @@ function run {
 
 run xautolock -detectsleep -notify 5 -notifier "xset dpms force off" -time 20 -locker "slock" -killtime 20 -killer "systemctl suspend"
 run redshift -l 45.566267:5.920364
+run picom
