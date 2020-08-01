fe406bf926ce030aed3cc7c7932a8fb46a550297
Simon LÉONARD <Matows@users.noreply.github.com>
2020-08-01 14:27:30 +0200

    xautolock now use suspend-then-hibernate, not suspend

--- .config/awesome/config/apps.lua
+++ .config/awesome/config/apps.lua
@@ -1,6 +1,6 @@
 -- Command to start once at startup
 return {
-    'xautolock -detectsleep -notify 5 -notifier "xset dpms force off" -time 20 -locker "slock" -killtime 20 -killer "systemctl suspend"',
+    'xautolock -detectsleep -notify 5 -notifier "xset dpms force off" -time 20 -locker "slock" -killtime 20 -killer "systemctl suspend-then-hibernate"',
     'redshift -l 45.566267:5.920364',
     'picom'
 }
