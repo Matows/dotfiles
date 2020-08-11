5424af592371bfbc7195c938247673c202fb2acf
Simon LÉONARD <Matows@users.noreply.github.com>
2020-08-12 00:41:05 +0200

    now using lightdm, updating xautolock

--- .config/awesome/config/apps.lua
+++ .config/awesome/config/apps.lua
@@ -1,6 +1,10 @@
 -- Command to start once at startup
 return {
-    'xautolock -detectsleep -notify 5 -notifier "xset dpms force off" -time 20 -locker "slock" -killtime 20 -killer "systemctl suspend-then-hibernate"',
+    'light-locker',
+    'xautolock -detectsleep \z
+    -notify 60 -notifier "xset dpms force off" \z
+    -time 10 -locker "dm-tool lock" \z
+    -killtime 20 -killer "systemctl suspend-then-hibernate"',
     'redshift -l 45.566267:5.920364',
     'picom'
 }
